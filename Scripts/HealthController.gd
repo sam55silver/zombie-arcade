af312440e7bdac684d3d3d34a238d0aeea6e755f
Sam Silver <sam55silver@gmail.com>
2022-08-15 11:59:50 -0300

    Added base code for player death

--- Scripts/HealthController.gd
+++ Scripts/HealthController.gd
@@ -5,9 +5,13 @@ var health = 8;
 var heart_full = preload("res://Art/Health UI/HealthUI -Health Unit.png")
 var heart_empty = preload("res://Art/Health UI/HealthUI - Empty Health Unit.png")
 
+signal player_dead()
+onready var player = get_node("../../Player")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	health = 8
+	self.connect("player_dead", player, "_on_Player_Die")
 
 func get_hit():
 	health -= 1
@@ -22,7 +26,7 @@ func get_hit():
 		player_dead()
 
 func player_dead():
-	print("dead")
+	emit_signal("player_dead")
 
 func _on_Player_got_hurt():
 	get_hit()
--- Scripts/PlayerController.gd
+++ Scripts/PlayerController.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 var player_is_invs = false
+var player_is_dead = false
 
 const MAX_SPEED = 500.0
 const ACCELERATION = 100
@@ -13,6 +14,7 @@ signal hurt_player()
 onready var health = get_node("../UI/HealthContainer")
 
 func _ready():
+	_animated_sprite.play("Idle")
 	self.connect("hurt_player", health, "_on_Player_got_hurt")
 
 func _process(_delta):
@@ -23,27 +25,31 @@ func _process(_delta):
 		get_parent().add_child(new_bullet)
 
 func _physics_process(delta):
-	look_at(get_global_mouse_position())
+	if !player_is_dead:
+		look_at(get_global_mouse_position())
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Vector2.ZERO
-	direction.x = Input.get_axis("move_left", "move_right")
-	direction.y = Input.get_axis("move_up", "move_down")
-	direction = direction.normalized()
-	
-	if direction:
-		velocity += direction * ACCELERATION  
-		velocity = velocity.clamped(MAX_SPEED)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, 1000 * delta)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		var direction = Vector2.ZERO
+		direction.x = Input.get_axis("move_left", "move_right")
+		direction.y = Input.get_axis("move_up", "move_down")
+		direction = direction.normalized()
 		
-	move_and_slide(velocity)
+		if direction:
+			velocity += direction * ACCELERATION  
+			velocity = velocity.clamped(MAX_SPEED)
+		else:
+			velocity = velocity.move_toward(Vector2.ZERO, 1000 * delta)
+			
+		move_and_slide(velocity)
 
 
 func _on_AnimatedSprite_animation_finished():
 	if(_animated_sprite.get_animation() == "Shoot"):
 		_animated_sprite.play("Idle")
+	
+	if("Death" in _animated_sprite.get_animation()):
+		get_tree().change_scene("res://Nodes/Scenes/game_over.tscn")
 		
 func _on_Player_Zombie_Collide():
 	if !player_is_invs:
@@ -53,3 +59,8 @@ func _on_Player_Zombie_Collide():
 
 func _on_Timer_timeout():
 	player_is_invs = false
+
+func _on_Player_Die():
+	player_is_dead = true
+	_animated_sprite.set_offset(Vector2(-4,-84))
+	_animated_sprite.play("Death-1")
