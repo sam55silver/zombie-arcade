8e14c9f33338b39a2d1bb9d88deff0347a7b957f
Sam Silver <sam55silver@gmail.com>
2022-08-19 14:23:59 -0300

    Changed styling of high score scene

--- Scripts/high_score.gd
+++ Scripts/high_score.gd
@@ -6,18 +6,26 @@ func _ready():
 	var size = globals.high_scores.size()
 	
 	for n in 8:
-		var score_label = Label.new()
-		var score_string = "{index}. ".format({"index": n + 1})
+		var score_index = Label.new()
+		score_index.set_text("{index}.".format({"index": n + 1}))
+		
+		var score_name = Label.new()
+		var score_score = Label.new()
 		
 		if size > n:
 			var score = globals.high_scores[n]
 			var three_char_score = globals.count_to_string(score.score)
-			score_label.set_text(score_string + "{name}------------{score}".format({"name": score.name, "score": three_char_score}))
+			
+			score_name.set_text(score.name)
+			score_score.set_text(three_char_score)
 		
 		else:
-			score_label.set_text(score_string + "---------------------")
+			score_name.set_text("-----")
+			score_score.set_text("---")
 		
-		$high_scores.add_child(score_label)
+		$high_scores.add_child(score_index)
+		$high_scores.add_child(score_name)
+		$high_scores.add_child(score_score)
 
 func _on_retryButton_button_up():
 	get_tree().change_scene("res://Nodes/Scenes/game_world.tscn")
