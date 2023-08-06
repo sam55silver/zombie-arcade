bef587c57f059f9667edafb4945b8a9584a89d84
Sam Silver <sam55silver@gmail.com>
2023-08-06 15:44:28 -0300

    Finished slow zombie turn

--- src/Game/Components/Zombie.js
+++ src/Game/Components/Zombie.js
@@ -10,14 +10,20 @@ class Zombie extends CharacterController {
       6,
       [scene.spriteSheet.textures[`zombie-${type}.png`]],
       { x: 0.5, y: 0.9 },
-      0.8
+      0.6
     );
 
     this.setHitBoxOffset({ x: 4, y: 4 });
 
+    this.sprite.rotation = Math.PI / 2
+
     this.health = 2;
-    this.lookAtPlayer();
     this.alreadyHit = false;
+
+    this.rotation = this.lookAt(
+      this.hitBox.pos,
+      this.scene.player.hitBox.pos
+    ).angle - Math.PI / 2;
   }
 
   hit() {
@@ -49,25 +55,26 @@ class Zombie extends CharacterController {
       };
     }
   }
-
+ 
   lookAtPlayer() {
-    const lookAtPlayer = this.lookAt(
-      this.hitBox.pos,
-      this.scene.player.hitBox.pos
-    );
+    const currentAngle = this.rotation
+    
+    const target = Math.atan2(this.scene.player.hitBox.pos.y - this.hitBox.pos.y,this.scene.player.hitBox.pos.x - this.hitBox.pos.x)
 
-    const rads = lookAtPlayer.angle - Math.PI / 2
+    let angleDifference = target - currentAngle;
 
-    if (lookAtPlayer.angle != this.rotation) {
-      console.log("zombir angle:", rads, "2pi", Math.PI)
-
-
+    const diff = 2 * Math.PI 
+    while (angleDifference > 2 * Math.PI / 2) {
+      angleDifference -= diff;
+    }
+    while (angleDifference < -Math.PI / 2) {
+      angleDifference += diff
     }
 
+    const lerpFactor = 0.035
+    const newAngle = currentAngle + lerpFactor * angleDifference
 
-
-
-    return lookAtPlayer;
+    this.rotation = newAngle
   }
 
   testCollideWithZombies() {
@@ -91,6 +98,14 @@ class Zombie extends CharacterController {
   updateCharacter(delta) {
     if (!(this.health <= 0)) {
       this.lookAtPlayer()
+
+      const x = Math.cos(this.rotation)
+      const y = Math.sin(this.rotation)
+      console.log(x, y, this.hitBox.pos)
+
+      this.velocity = new SAT.Vector(x,y)
+
+
       // this.velocity = this.lookAtPlayer().vectorTo.normalize();
       //
       this.rigidBodyCollisionCheck(
